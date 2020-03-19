d71b6119f72352476e34561565e7d7a6beb3386d
UTPAL-GAURAV <utpal38bpc@gmail.com>
2020-03-19 15:18:39 +0530

    Magic Eight Balls Challenge Compleated

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,38 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageView img = findViewById(R.id.image_eightBall);
+        final int[] arr1 = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+
+        final Button ask_button = findViewById(R.id.ask_button);
+        ask_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Random randomNumberGenerator = new Random();
+                int rannum = randomNumberGenerator.nextInt(4);
+
+                img.setImageResource(arr1[rannum]);
+            }
+        });
+
+
     }
 }
